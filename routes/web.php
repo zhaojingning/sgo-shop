fe5275e4ed7458302cb710d837da24942bd46479
zhaojingning <784955168@qq.com>
2023-04-08 14:51:48 +0800

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    .
+    Route::post('cart', 'CartController@add')->name('cart.add');
 });
 
 Route::redirect('/', '/products')->name('root');
