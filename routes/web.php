a6d888b35bb9bd5086482ad60ed3bce31ea97626
zhaojingning <784955168@qq.com>
2023-04-09 17:54:15 +0800

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -39,17 +39,23 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 });
+
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Route::get('alipay', function() {
-    return app('alipay')->web([
+    $alipay =  app('alipay')->web([
         'out_trade_no' => time(),
         'total_amount' => '1',
         'subject' => 'test subject - 测试',
     ]);
+    return view('pages.root', ['content' => $alipay]);
 });
 
