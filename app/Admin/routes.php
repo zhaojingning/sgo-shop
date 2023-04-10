952d8e6928bcff73177ed7f3a26f4fc1a9bb1ec6
vagrant <vagrant@homestead>
2023-04-10 10:52:33 +0000

    管理后台 - 新增优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -26,5 +26,7 @@ Route::group([
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('orders.handle_refund');
 
     $router->get('coupon_codes', 'CouponCodesController@index');
+    $router->post('coupon_codes', 'CouponCodesController@store');
+$router->get('coupon_codes/create', 'CouponCodesController@create');
 
 });
